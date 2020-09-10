7c11a50b1da5e83bd7369608eff04bd9e423ddc8
Martin Strøm Olsen <martsols@student.matnat.uio.no>
2020-09-10 13:50:40 +0200

    Add start of name_banker

--- src/project-1/name_banker.py
+++ src/project-1/name_banker.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class RandomBanker:
+class NameBanker:
     
     # Fit the model to the data.  You can use any model you like to do
     # the fit, however you should be able to predict all class
@@ -17,7 +17,10 @@ class RandomBanker:
     def predict_proba(self, x):
         return 0
 
-    # THe expected utility of granting the loan or not. Here there are two actions:
+    def get_proba(self):
+        return np.random.uniform(0,1)
+
+    # The expected utility of granting the loan or not. Here there are two actions:
     # action = 0 do not grant the loan
     # action = 1 grant the loan
     #
@@ -25,7 +28,17 @@ class RandomBanker:
     # 2nd attribute of x. Then the return if the loan is paid off to you is amount_of_loan*(1 + rate)^length_of_loan
     # The return if the loan is not paid off is -amount_of_loan.
     def expected_utility(self, x, action):
-        print("Expected utility: Not implemented")
+        """Calculate expected utility.
+
+        """
+        r = self.rate
+        p_c = self.get_proba()
+        n = x.length_of_loan
+        m = x.amount_of_loan
+        
+        e_x = p_c*m*((1+r)**n-1) + (1-p_c)*(-m)
+        return e_x
+
     # Return the best action. This is normally the one that maximises expected utility.
     # However, you are allowed to deviate from this if you can justify the reason.
     def get_best_action(self, x):
