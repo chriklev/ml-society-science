27ac3b8ca3c6f0691c509d7d62d2b8d2fbee76c9
Martin Strøm Olsen <martsols@student.matnat.uio.no>
2020-09-24 12:30:21 +0200

    Update test impl.

--- src/project-1/TestImplementation.py
+++ src/project-1/TestImplementation.py
@@ -1,16 +1,8 @@
 import random_banker
 import name_banker
-from sklearn import train_test_split
+from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
-
-if __name__ == "__main__":
-    features = ['checking account balance', 'duration', 'credit history',
-            'purpose', 'amount', 'savings', 'employment', 'installment',
-            'marital status', 'other debtors', 'residence time',
-            'property', 'age', 'other installments', 'housing', 'credits',
-            'job', 'persons', 'phone', 'foreign', 'repaid']
-    response = 'repaid'
 
 def get_data():
     features = ['checking account balance', 'duration', 'credit history',
@@ -54,15 +46,19 @@ def utility_from_test_set(X, y, decision_maker, interest_rate):
         amount = X['amount'].iloc[new_obs]
         duration = X['duration'].iloc[new_obs]
         
-    pass
-    
+        obs_utility[new_obs] = utility_from_obs(predicted_decision, true_decision, amount, duration, interest_rate)
+        obs_amount[new_obs] = amount
 
-def compare_decision_makers(num_of_tests, covariates, response, interest_rate):
+    return np.sum(obs_utility), np.sum(obs_utility)/np.sum(obs_amount)
+
+def compare_decision_makers(num_of_tests, response, interest_rate):
     """Tests the random banker against our name banker.
 
     """
-    bank_utility = np.zeros(num_of_tests)
-    bank_investment = np.zeros_like(bank_utility)
+    bank_utility_random = np.zeros(num_of_tests)
+    bank_investment_random = np.zeros_like(bank_utility_random)
+    bank_utility_name = np.zeros(num_of_tests)
+    bank_investment_name = np.zeros_like(bank_utility_name)
 
     # decision makers
     r_banker = random_banker.RandomBanker()
@@ -72,6 +68,7 @@ def compare_decision_makers(num_of_tests, covariates, response, interest_rate):
 
     # get data
     X = get_data()
+    covariates = X.columns[X.columns != response]
     
     for i in range(num_of_tests):
         X_train, X_test, y_train, y_test = train_test_split(X[covariates], X[response], test_size = 0.2)
@@ -79,7 +76,18 @@ def compare_decision_makers(num_of_tests, covariates, response, interest_rate):
         # fit models
         r_banker.fit(X_train, y_train)
         n_banker.fit(X_train, y_train)
+        
+        bank_utility_random[i], bank_investment_random[i] = utility_from_test_set(X_test, y_test, r_banker, interest_rate)
+        bank_utility_name[i], bank_investment_name[i] = utility_from_test_set(X_test, y_test, n_banker, interest_rate)
 
+    print(f"Avg. utility [random]= {np.sum(bank_utility_random)/num_of_tests}")
+    print(f"Avg. ROI [random] {np.sum(bank_investment_random)/num_of_tests}")
+    print(f"Avg. utility [name]= {np.sum(bank_utility_name)/num_of_tests}")
+    print(f"Avg. ROI [name] {np.sum(bank_investment_name)/num_of_tests}")
 
 
         
+if __name__ == "__main__":
+    response = 'repaid'
+
+    compare_decision_makers(100, response, 0.05)
