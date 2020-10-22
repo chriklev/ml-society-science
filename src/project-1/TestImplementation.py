3c3feabe679891bcb450c93d9a8ecf9da7ec04ae
Martin Strøm Olsen <martsols@student.matnat.uio.no>
2020-10-22 19:33:01 +0200

    Add fairness balance calc.

--- src/project-1/TestImplementation.py
+++ src/project-1/TestImplementation.py
@@ -133,6 +133,165 @@ def repeated_cross_validation_utility(X, y, bankers, banker_names, interest_rate
     return results
 
 
+def _calculate_balance(df, threshold=None, upper=True):
+    """Calculates the probability for the balance metric using relative
+    frequency.
+
+    Args:
+        df: dataframe containing
+            a: the action taken by the algorithm
+            y: the true response
+            z: the gender of the observation
+            am: the amount of loan
+        threshold: whether or not to threshold the amount
+        upper: use upper part of threshold
+    Returns:
+        Calculated balance loss.
+    """
+    if (threshold == None):
+        N = len(df)
+        bal = 0
+        for a in range(0, 2):
+            for y in range(0, 2):
+                p_y = len(df[df['y'] == y])/N
+                p_a_and_y = len(df[(df['a'] == a) & (df['y'] == y)])/N
+                p_a_y = p_a_and_y/p_y
+                for z in range(0, 2):
+                    p_y_z = len(df[(df['y'] == y) & (df['z'] == z)])/N
+                    p_a_and_y_and_z = len(
+                        df[(df['a'] == a) & (df['y'] == y) & (df['z'] == z)])/N
+                    p_a_y_z = p_a_and_y_and_z/p_y_z
+                    bal += (p_a_y_z - p_a_y)**2
+
+        return bal
+    elif upper:
+        N = len(df['am'] > threshold)
+        bal = 0
+        for a in range(0, 2):
+            for y in range(0, 2):
+                p_y = len(df[(df['y'] == y) & (df['am'] > threshold)])/N
+                p_a_and_y = len(df[(df['a'] == a) & (
+                    df['y'] == y) & (df['am'] > threshold)])/N
+                p_a_y = p_a_and_y/p_y
+                for z in range(0, 2):
+                    p_y_z = len(df[(df['y'] == y) & (
+                        df['z'] == z) & (df['am'] > threshold)])/N
+                    p_a_and_y_and_z = len(
+                        df[(df['a'] == a) & (df['y'] == y) & (df['z'] == z) & (df['am'] > threshold)])/N
+                    p_a_y_z = p_a_and_y_and_z/p_y_z
+                    bal += (p_a_y_z - p_a_y)**2
+        return bal
+    else:
+        N = len(df['am'] <= threshold)
+        bal = 0
+        for a in range(0, 2):
+            for y in range(0, 2):
+                p_y = len(df[(df['y'] == y) & (df['am'] <= threshold)])/N
+                p_a_and_y = len(df[(df['a'] == a) & (
+                    df['y'] == y) & (df['am'] <= threshold)])/N
+                p_a_y = p_a_and_y/p_y
+                for z in range(0, 2):
+                    p_y_z = len(df[(df['y'] == y) & (df['z'] == z)
+                                   & (df['am'] <= threshold)])/N
+                    p_a_and_y_and_z = len(
+                        df[(df['a'] == a) & (df['y'] == y) & (df['z'] == z) & (df['am'] <= threshold)])/N
+                    p_a_y_z = p_a_and_y_and_z/p_y_z
+                    bal += (p_a_y_z - p_a_y)**2
+        return bal
+
+
+def repeated_cv_fairness(X, y, banker, n_repeats=10, n_folds=10):
+    """Calculates various fairness metrics with a repeated k-fold cross
+    validation.
+
+    Args:
+        X: covariates
+        y: response variable
+        n_repeats: repetitions of k-fold CV
+        n_folds: number of folds to use in CV
+    Returns:
+        A dictionary of the fairness results.
+    """
+    amount_threshold = np.median(X['amount'])
+
+    fairness_results = {}
+    total_var_dists_y1 = np.zeros(n_repeats*n_folds)
+    total_var_dists_y0 = np.zeros(n_repeats*n_folds)
+    total_fairness_bal = np.zeros(n_repeats*n_folds)
+    total_fairness_bal_low = np.zeros(n_repeats*n_folds)
+    total_fairness_bal_high = np.zeros(n_repeats*n_folds)
+    t = 0
+
+    for i in range(n_repeats):
+        kf = KFold(n_splits=n_folds, shuffle=True)
+        for train_indices, test_indices in kf.split(X):
+            X_train = X.iloc[train_indices, :]
+            X_test = X.iloc[test_indices, :]
+            y_train = y[train_indices]
+            y_test = y[test_indices]
+
+            # fit model
+            banker.fit(X_train, y_train)
+
+            num_obs = len(X_test)
+            a_obs = np.zeros(num_obs)
+            am_obs = np.zeros(num_obs)
+            y_obs = np.zeros(num_obs)
+            z_obs = np.zeros(num_obs)
+
+            for new_obs in range(num_obs):
+                obs = X_test.iloc[new_obs]
+
+                a_i = banker.get_best_action(obs)
+                z_i = _get_gender(obs)
+                y_i = y_test.iloc[new_obs]
+                am_i = X.iloc[new_obs]['amount']
+
+                a_obs[new_obs] = a_i
+                y_obs[new_obs] = y_i
+                z_obs[new_obs] = z_i
+                am_obs[new_obs] = am_i
+
+            fairness_df = pd.DataFrame(
+                {'z': list(z_obs), 'a': list(a_obs), 'y': list(y_obs), 'am': list(am_obs)})
+
+            men = fairness_df.loc[fairness_df['z'] == 1]
+            women = fairness_df.loc[fairness_df['z'] == 0]
+
+            z1_y1_a1 = len(men[(men['y'] == 1) & (
+                men['a'] == 1)])/len(men[men['y'] == 1])
+            z1_y0_a1 = len(men[(men['y'] == 0) & (
+                men['a'] == 1)]) / len(men[men['y'] == 0])
+            z0_y1_a1 = len(women[(women['y'] == 1) & (
+                women['a'] == 1)])/len(women[women['y'] == 1])
+            z0_y0_a1 = len(women[(women['y'] == 0) & (
+                women['a'] == 1)])/len(women[women['y'] == 0])
+
+            prob_m_y1 = np.array([z1_y1_a1, 1-z1_y1_a1])
+            prob_w_y1 = np.array([z0_y1_a1, 1-z0_y1_a1])
+
+            prob_m_y0 = np.array([z1_y0_a1, 1-z1_y0_a1])
+            prob_w_y0 = np.array([z0_y0_a1, 1-z0_y0_a1])
+
+            total_var_dists_y1[t] = total_variation(prob_m_y1, prob_w_y1)
+            total_var_dists_y0[t] = total_variation(prob_m_y0, prob_w_y0)
+
+            total_fairness_bal[t] = _calculate_balance(fairness_df)
+            total_fairness_bal_low[t] = _calculate_balance(
+                fairness_df, threshold=amount_threshold, upper=False)
+            total_fairness_bal_high[t] = _calculate_balance(
+                fairness_df, threshold=amount_threshold, upper=True)
+            t = t + 1
+
+    fairness_results['tv0'] = total_var_dists_y0
+    fairness_results['tv1'] = total_var_dists_y1
+    fairness_results['fair_balance'] = total_fairness_bal
+    fairness_results['fair_low'] = total_fairness_bal_low
+    fairness_results['fair_high'] = total_fairness_bal_high
+
+    return fairness_results
+
+
 def compare_decision_makers(n_repeats, n_folds, response, interest_rate):
     """Tests the random banker against our group1 banker.
 
@@ -308,6 +467,15 @@ def _get_priors(model):
 
 
 def _get_likelihood(model, X, y_values):
+    """Gets the log-likelihood for the data given the model.
+
+    Args:
+        model: the probability model used
+        X: the covariates
+        y_values: the response values
+    Returns:
+        The log-likelihood
+    """
     log_probs = model.predict_log_proba(X)[:, 0]
     log_lik = 0
 
@@ -322,6 +490,7 @@ def fairness(response, interest_rate=0.05):
 
     Args:
         response: name of response variable in the data set
+        interest_rate: the interest rate to use
     """
     data = get_data()
     y = data.pop(response)
@@ -330,85 +499,26 @@ def fairness(response, interest_rate=0.05):
     g_banker = group1_banker.Group1Banker()
     g_banker.set_interest_rate(interest_rate)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, random_state=1, test_size=0.25)
+    fairness_results = repeated_cv_fairness(
+        X, y, g_banker, n_repeats=10, n_folds=10)
 
-    g_banker.fit(X_train, y_train)
+    print(f"TVD y=1 = {np.mean(fairness_results['tv1'])}")
+    print(f"TVD y=0 = {np.mean(fairness_results['tv0'])}")
 
-    num_obs = len(X_test)
-    a = np.zeros(num_obs)
-    y = np.zeros(num_obs)
-    z = np.zeros(num_obs)
-
-    for new_obs in range(num_obs):
-        obs = X_test.iloc[new_obs]
-
-        a_i = g_banker.get_best_action(obs)
-        z_i = _get_gender(obs)
-        y_i = y_test.iloc[new_obs]
-
-        a[new_obs] = a_i
-        y[new_obs] = y_i
-        z[new_obs] = z_i
-
-    fairness_df = pd.DataFrame({'z': list(z), 'a': list(a), 'y': list(y)})
-
-    men = fairness_df.loc[fairness_df['z'] == 1]
-    women = fairness_df.loc[fairness_df['z'] == 0]
-
-    z1_y1_a1 = len(men[(men['y'] == 1) & (men['a'] == 1)]) / \
-        len(men[men['y'] == 1])
-    z1_y0_a1 = len(men[(men['y'] == 0) & (men['a'] == 1)]) / \
-        len(men[men['y'] == 0])
-    z0_y1_a1 = len(women[(women['y'] == 1) & (
-        women['a'] == 1)])/len(women[women['y'] == 1])
-    z0_y0_a1 = len(women[(women['y'] == 0) & (
-        women['a'] == 1)])/len(women[women['y'] == 0])
-
-    prob_m_y1 = np.array([z1_y1_a1, 1-z1_y1_a1])
-    prob_w_y1 = np.array([z0_y1_a1, 1-z0_y1_a1])
-
-    prob_m_y0 = np.array([z1_y0_a1, 1-z1_y0_a1])
-    prob_w_y0 = np.array([z0_y0_a1, 1-z0_y0_a1])
-
-    total_var_dist_y1 = total_variation(prob_m_y1, prob_w_y1)
-    total_var_dist_y0 = total_variation(prob_m_y0, prob_w_y0)
-
-    print(f"TVD y=1 = {total_var_dist_y1}")
-    print(f"TVD y=0 = {total_var_dist_y0}")
-
-
-def stochastic_gradient_descent(response, interest_rate):
-    data = get_data()
-    y = data.pop(response)
-    X = data
-
-    g_banker = group1_banker.Group1Banker()
-    g_banker.set_interest_rate(interest_rate)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, random_state=1, test_size=0.25)
-
-    g_banker.fit(X_train, y_train)
-
-    log_model = g_banker.model
-
-    # priors
-    priors = _get_priors(log_model)
-
-    # the likelihood of the data
-    log_likelihood = _get_likelihood(log_model, X_test, y_test.to_numpy())
-    likelihood = tfp.distributions.Deterministic(np.exp(log_likelihood))
-
-    # the posterior
-    posterior = tfp.distributions.JointDistributionNamed({
-        'p': priors
-    })
-
-    breakpoint()
+    print(f"F_balance = {np.mean(fairness_results['fair_balance'])}")
+    print(f"F_balance low = {np.mean(fairness_results['fair_low'])}")
+    print(f"F_balance high = {np.mean(fairness_results['fair_high'])}")
 
 
 def total_variation(prob1, prob2):
+    """Calculates the total variation distance by using the formula from
+    Wikipedia as referenced in the exercise text.
+
+    Args:
+        prob1: probability for a=1 for male (z=1)
+        prob2: probability for a=1 for female (z=0)
+
+    """
     return (1/2)*np.sum(np.abs(prob1 - prob2))
 
 
@@ -418,7 +528,6 @@ if __name__ == "__main__":
     np.random.seed(1)
     response = 'repaid'
     fairness(response)
-    stochastic_gradient_descent(response, 0.05)
     """
     results = compare_decision_makers(
         n_repeats=20, n_folds=5, response=response, interest_rate=0.05)
