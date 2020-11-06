2e81114108fecdde70d5b8f2164731073d874031
Martin Strøm Olsen <martsols@student.matnat.uio.no>
2020-11-06 10:23:48 +0100

    Add project 2

---
+++ src/project-2/part1.py
@@ -0,0 +1,175 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.feature_selection import RFECV
+from sklearn.linear_model import LogisticRegression
+
+
+class MedicalData:
+    def __init__(self):
+        self._get_data()
+
+    def _get_data(self):
+        """Gets the data and places it into three dataframes stored as instance
+        variables of the object.
+        """
+        x = pd.read_csv("./data/medical/historical_X.dat",
+                        sep=" ", header=None)
+        personal_columns = ["sex", "smoker"]
+        gene_columns = ["gene " + str(i) for i in range(1, 127)]
+        symptom_columns = ["symptom 1", "symptom 2"]
+        x_columns = personal_columns + gene_columns + symptom_columns
+        x.columns = x_columns
+
+        y = pd.read_csv("./data/medical/historical_Y.dat",
+                        sep=" ", header=None)
+        y.columns = ["outcome"]
+
+        a = pd.read_csv("./data/medical/historical_A.dat",
+                        sep=" ", header=None)
+        a.columns = ["action"]
+
+        self.x_train, self.x_test, self.y_train, self.y_test, self.a_train, self.a_test = train_test_split(
+            x, y, a, test_size=0.3, random_state=1)
+        # breakpoint()
+
+    def data_analysis(self):
+        """
+
+        """
+        self.frequency_symptoms()
+        self.variable_selection(3)
+
+    def _plot_variable_selection(self, accuracy_score1, accuracy_score2, show=False):
+        """Plots the accuracy score for the different symptoms.
+
+        Args:
+            accuracy_score1: the accuracy score from the CV of symptom 1
+            accuracy_score2: the accuracy score from the CV of symptom 2
+            show: whether or not to show the plot
+        """
+        fig, (axis1, axis2) = plt.subplots(1, 2)
+        fig.suptitle("Accuracy score for variable selection")
+
+        axis1.plot(range(1, len(accuracy_score1) + 1), accuracy_score1)
+        axis1.set_title("Symptom 1")
+        axis1.set_ylabel("Accuracy score")
+        axis1.set_xlabel("Number of covariates")
+        axis2.plot(range(1, len(accuracy_score2) + 1), accuracy_score2)
+        axis2.set_title("Symptom 2")
+        axis2.set_xlabel("Number of covariates")
+
+        if show:
+            plt.show()
+        else:
+            plt.savefig("img/var_sel.png")
+
+    def variable_selection(self, num_folds):
+        """Performs variable selection using a num_folds cross-validation.
+
+        RFECV adapted from https://scikit-learn.org/stable/auto_examples/
+        feature_selection/plot_rfe_with_cross_validation.html#sphx-glr-auto-
+        examples-feature-selection-plot-rfe-with-cross-validation-py
+
+        Args:
+            num_folds: the number of folds to use in the cross-validation
+        """
+        logistic_regression = LogisticRegression(max_iter=1000)
+        variable_selection_cv = RFECV(
+            estimator=logistic_regression, step=1, cv=StratifiedKFold(num_folds, random_state=1), scoring='accuracy')
+
+        x = self.x_train.iloc[:, : -2]
+        symptom1 = self.x_train.iloc[:, -2]
+        symptom2 = self.x_train.iloc[:, -1]
+
+        # symptom 1
+        variable_selection_cv.fit(x, symptom1)
+        accuracy_symptom1 = variable_selection_cv.grid_scores_
+        symptom1_indices = np.where(
+            variable_selection_cv.support_ == True)[0]
+        print(f"Symptom 1 covariates = {x.columns[symptom1_indices]}")
+
+        # symptom 2
+        variable_selection_cv.fit(x, symptom2)
+        accuracy_symptom2 = variable_selection_cv.grid_scores_
+        symptom2_indices = np.where(
+            variable_selection_cv.support_ == True)[0]
+        print(f"Symptom 2 covariates = {x.columns[symptom2_indices]}")
+
+        self._plot_variable_selection(accuracy_symptom1, accuracy_symptom2)
+
+    def frequency_symptoms(self, show=False):
+        """Generates simple histogram showing the frequency of the different 
+        symptoms. Looks at the entire dataset when considering the frequency.
+
+        Args:
+            show: whether or not to show the plot
+        """
+        x_joined = [self.x_train, self.x_test]
+        x = pd.concat(x_joined)
+
+        fig, (axis1, axis2) = plt.subplots(1, 2)
+        fig.suptitle("Histogram of symptoms")
+        axis1.hist(x["symptom 1"], color='b')
+        axis1.set_title("Symptom 1")
+        axis2.hist(x["symptom 2"], color='r')
+        axis2.set_title("Symptom 2")
+
+        if show:
+            plt.show()
+        else:
+            plt.savefig("img/freq_hist.png")
+
+    def measure_effect(self, action):
+        """Calculates the measured effect of an action.
+
+        Args:
+            action: 1 for treatment and 0 for placebo
+
+        Returns:
+            The measured effect.
+        """
+        y_joined = [self.y_train, self.y_test]
+        y = pd.concat(y_joined)
+        y_array = self._to_flat_array(y)
+
+        a_joined = [self.a_train, self.a_test]
+        a = pd.concat(a_joined)
+        a_array = self._to_flat_array(a)
+
+        return self._utility(a_array, y_array, action)
+
+    def _to_flat_array(self, df):
+
+        numpy_array = df.to_numpy()
+        return numpy_array.flatten()
+
+    def _utility(self, a, y, at):
+        """Calculates utility.
+
+        Args:
+            a: action array
+            y: outcome array
+            at: action to measure utility for
+
+        Returns:
+            Utility for observation.
+        """
+        num_at = len(np.where(a == at)[0])
+        u = 0
+
+        for i in range(len(a)):
+            if a[i] == at and y[i] == 1:
+                u += 1
+
+        return u/num_at
+
+
+if __name__ == "__main__":
+    data = MedicalData()
+    # data.data_analysis()
+    expected_utility_1 = data.measure_effect(1)
+    expected_utility_0 = data.measure_effect(0)
+    print(f"E[U|a_t = 1] = {expected_utility_1}")
+    print(f"E[U|a_t = 0] = {expected_utility_0}")
