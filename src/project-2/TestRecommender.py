d3e92a8859887f693445248a8a31538a9765554f
Christoffer Kleven Berg <cklevenb@gmail.com>
2020-11-19 11:20:58 +0100

    testing

--- src/project-2/TestRecommender.py
+++ src/project-2/TestRecommender.py
@@ -1,7 +1,13 @@
+import reference_recommender
+import random_recommender
+import data_generation
 import numpy as np
 import pandas
+
+
 def default_reward_function(action, outcome):
-    return -0.1 * (action!= 0) + outcome
+    return -0.1 * (action != 0) + outcome
+
 
 def test_policy(generator, policy, reward_function, T):
     print("Testing for ", T, "steps")
@@ -14,33 +20,34 @@ def test_policy(generator, policy, reward_function, T):
         r = reward_function(a, y)
         u += r
         policy.observe(x, a, y)
-        #print(a)
+        # print(a)
         print("x: ", x, "a: ", a, "y:", y, "r:", r)
     return u
 
-features = pandas.read_csv('data/medical/historical_X.dat', header=None, sep=" ").values
-actions = pandas.read_csv('data/medical/historical_A.dat', header=None, sep=" ").values
-outcome = pandas.read_csv('data/medical/historical_Y.dat', header=None, sep=" ").values
-observations = features[:, :128]
-labels = features[:,128] + features[:,129]*2
 
-import data_generation
-import random_recommender
+features = pandas.read_csv(
+    'data/medical/historical_X.dat', header=None, sep=" ").values
+actions = pandas.read_csv(
+    'data/medical/historical_A.dat', header=None, sep=" ").values
+outcome = pandas.read_csv(
+    'data/medical/historical_Y.dat', header=None, sep=" ").values
+observations = features[:, :128]
+labels = features[:, 128] + features[:, 129]*2
+
 policy_factory = random_recommender.RandomRecommender
-import reference_recommender
 #policy_factory = reference_recommender.HistoricalRecommender
 
-## First test with the same number of treatments
+# First test with the same number of treatments
 print("---- Testing with only two treatments ----")
-
 print("Setting up simulator")
-generator = data_generation.DataGenerator(matrices="./big_generating_matrices.mat")
+generator = data_generation.DataGenerator(
+    matrices="./big_generating_matrices.mat")
 print("Setting up policy")
 policy = policy_factory(generator.get_n_actions(), generator.get_n_outcomes())
-## Fit the policy on historical data first
+# Fit the policy on historical data first
 print("Fitting historical data to the policy")
 policy.fit_treatment_outcome(features, actions, outcome)
-## Run an online test with a small number of actions
+# Run an online test with a small number of actions
 print("Running an online test")
 n_tests = 1000
 result = test_policy(generator, policy, default_reward_function, n_tests)
@@ -48,23 +55,20 @@ print("Total reward:", result)
 print("Final analysis of results")
 policy.final_analysis()
 
-## First test with the same number of treatments
+# First test with the same number of treatments
 print("--- Testing with an additional experimental treatment and 126 gene silencing treatments ---")
 print("Setting up simulator")
-generator = data_generation.DataGenerator(matrices="./big_generating_matrices.mat")
+generator = data_generation.DataGenerator(
+    matrices="./big_generating_matrices.mat")
 print("Setting up policy")
 policy = policy_factory(generator.get_n_actions(), generator.get_n_outcomes())
-## Fit the policy on historical data first
+# Fit the policy on historical data first
 print("Fitting historical data to the policy")
 policy.fit_treatment_outcome(features, actions, outcome)
-## Run an online test with a small number of actions
+# Run an online test with a small number of actions
 print("Running an online test")
 n_tests = 1000
 result = test_policy(generator, policy, default_reward_function, n_tests)
 print("Total reward:", result)
 print("Final analysis of results")
 policy.final_analysis()
-
-
-
-
