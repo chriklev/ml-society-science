f07c6b6a0f51f4c29d02a160333ce21103109901
Christoffer Kleven Berg <cklevenb@gmail.com>
2020-12-01 09:43:09 +0100

    expected utility and live model

--- src/project-2/logistic_recommender.py
+++ src/project-2/logistic_recommender.py
@@ -36,26 +36,19 @@ class LogisticRecommender:
         self.n_actions = n_actions
         self.n_outcomes = n_outcomes
         self.reward = self._default_reward
+        self.models = None
 
     # By default, the reward is just equal to the outcome, as the actions play no role.
     def _default_reward(self, action, outcome):
         return outcome
 
-    # Set the reward function r(a, y)
     def set_reward(self, reward):
-        self.reward = reward
+        """ Set the reward function r(a, y)
 
-    ##################################
-    # Fit a model from patient data.
-    #
-    # This will generally speaking be an
-    # unsupervised model. Anything from a Gaussian mixture model to a
-    # neural network is a valid choice.  However, you can give special
-    # meaning to different parts of the data, and use a supervised
-    # model instead.
-    def fit_data(self, data):
-        print("Preprocessing data")
-        return None
+        Args:
+            reward: a function calcultaing reward and taking the arguments (action, outcome)
+        """
+        self.reward = reward
 
     def fit_treatment_outcome(self, data, actions, outcome):
         """ Group the data by action and fit a logistic regression model for each possible action.
@@ -68,6 +61,10 @@ class LogisticRecommender:
             actions: numpy.array with preformed actions
             outcome: numpy.array with observed outcomes
         """
+        self.model_data = data
+        self.model_actions = actions
+        self.model_outcome = outcome
+
         self.models = np.empty(self.n_actions)
 
         for a in range(self.n_actions):
@@ -77,22 +74,34 @@ class LogisticRecommender:
                 penalty='l2', multi_class='ovr')
             self.models[a].fit(data[ind], outcome[ind])
 
-    # Estimate the utility of a specific policy from historical data (data, actions, outcome),
-    # where utility is the expected reward of the policy.
-    ##
-    # If policy is not given, simply use the average reward of the observed actions and outcomes.
-    ##
-    # If a policy is given, then you can either use importance
-    # sampling, or use the model you have fitted from historical data
-    # to get an estimate of the utility.
-    ##
-    # The policy should be a recommender that implements get_action_probability()
-    def estimate_utility(self, data, actions, outcome, policy=None):
-        return 0
+    def estimate_utility(self, data, actions, outcome):
+        """ Calculates estimated utility for this reccomender.
 
-    # Return a distribution of effects for a given person's data and a specific treatment.
-    # This should be an numpy.array of length self.n_outcomes
+        Returns:
+            Estimated utility as a float.
+        """
+        self.fit_treatment_outcome(data, actions, outcome)
+
+        utility_total = 0
+        # For each observation
+        for i in range(len(outcome)):
+            # Find expected reward for each action
+            action_rewards = np.empty(self.n_actions)
+            for a in range(self.n_actions):
+                action_rewards[a] = self.reward(
+                    a, self.predict_proba(data[i, :], a))
+
+            utility_total += np.max(action_rewards)
+
+        return utility_total
+
     def predict_proba(self, data, treatment):
+        """ Predicts the distribution of outcomes given features and a treatment.
+
+        Args:
+            data: numpy.array with observed features.
+            treatment: integer representing the treatment.
+        """
         return self.models[treatment].predict_proba(data)[:, 1]
 
     def get_action_probabilities(self, user_data):
@@ -125,7 +134,9 @@ class LogisticRecommender:
     # Observe the effect of an action. This is an opportunity for you
     # to refit your models, to take the new information into account.
     def observe(self, user, action, outcome):
-        pass
+
+        user = user.reshape((1, self.model_data.shape[1]))
+        new_data = np.append(self.model_data, user)
 
     # After all the data has been obtained, do a final analysis. This can consist of a number of things:
     # 1. Recommending a specific fixed treatment policy
