d46ba48f4914f615a2f4ffa462a0b630be6227bc
Christoffer Kleven Berg <cklevenb@gmail.com>
2020-12-02 12:43:14 +0100

    updated TestRecommender to original version

--- src/project-2/TestRecommender.py
+++ src/project-2/TestRecommender.py
@@ -1,4 +1,6 @@
-import reference_recommender
+import adaptive_recommender
+import historical_recommender
+import improved_recommender
 import random_recommender
 import data_generation
 import numpy as np
@@ -21,7 +23,8 @@ def test_policy(generator, policy, reward_function, T):
         u += r
         policy.observe(x, a, y)
         # print(a)
-        print("x: ", x, "a: ", a, "y:", y, "r:", r)
+        #print("x: ", x, "a: ", a, "y:", y, "r:", r)
+        print(f"{t}/{T}")
     return u
 
 
@@ -34,14 +37,22 @@ outcome = pandas.read_csv(
 observations = features[:, :128]
 labels = features[:, 128] + features[:, 129]*2
 
-policy_factory = random_recommender.RandomRecommender
+#policy_factory = random_recommender.RandomRecommender
+
+#policy_factory = historical_recommender.HistoricalRecommender
+
+#policy_factory = adaptive_recommender.AdaptiveRecommender
+
+policy_factory = improved_recommender.ImprovedRecommender
+
+#import reference_recommender
 #policy_factory = reference_recommender.HistoricalRecommender
 
 # First test with the same number of treatments
 print("---- Testing with only two treatments ----")
+
 print("Setting up simulator")
-generator = data_generation.DataGenerator(
-    matrices="./big_generating_matrices.mat")
+generator = data_generation.DataGenerator(matrices="./generating_matrices.mat")
 print("Setting up policy")
 policy = policy_factory(generator.get_n_actions(), generator.get_n_outcomes())
 # Fit the policy on historical data first
