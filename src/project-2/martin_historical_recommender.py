696e3c5b2023f207d47c52b3514706277fc30324
Christoffer Kleven Berg <cklevenb@gmail.com>
2020-12-05 14:54:37 +0100

    clases from master

---
+++ src/project-2/martin_historical_recommender.py
@@ -0,0 +1,307 @@
+from sklearn import linear_model
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from part1 import MedicalData
+from utilities import RecommenderModel, FinalAnalysis, FixedTreatmentPolicy
+import pandas as pd
+import attr
+
+
+@attr.s
+class Approach1_hist_bl(RecommenderModel):
+
+    def fit_treatment_outcome(self, data, actions, outcomes):
+        """Fits the model used from historical data.
+
+        Args:
+            data: x_t
+            actions: a_t | x_t
+            outcomes: y_t | a_t, x_t
+        """
+
+        self.actions = actions
+        self.outcome = outcomes
+        self.data = data
+
+        x = np.hstack((data.copy(), actions))
+        regression_model = LogisticRegression(max_iter=5000, n_jobs=-1)
+
+        regression_model.fit(x, outcomes.flatten())
+        self.model = regression_model
+
+        policy_model = LogisticRegression(max_iter=5000, n_jobs=-1)
+        policy_model.fit(data, actions.flatten())
+        self.policy = policy_model
+
+    def get_action_probabilities(self, user_data):
+        # print("Recommending")
+        if isinstance(user_data, pd.core.series.Series):
+            user_data = user_data.to_numpy().reshape(1, -1)
+        else:
+            user_data = user_data.reshape(1, -1)
+
+        # pi(a|x)
+        pi = np.zeros(self.n_actions)
+
+        # predict values for a
+        predictions = self.policy.predict_proba(user_data)
+
+        for a_t in range(len(predictions[0])):
+            pi[a_t] = predictions[0][a_t]
+
+        assert np.sum(pi) == 1
+
+        return pi
+
+    def predict_proba(self, data, treatment):
+
+        if isinstance(data, pd.core.series.Series):
+            data['a'] = treatment
+            user_array = data.to_numpy().reshape(1, -1)
+        else:
+            user_array = np.hstack((data, treatment)).reshape(1, -1)
+
+        p = self.model.predict_proba(user_array)
+
+        return p[0]
+
+    def observe(self, user, action, outcome):
+        """Updates the data.
+
+        Args:
+            user: x_t
+            action: a_t
+            outcome: y_t
+        """
+        self.data = np.vstack((self.data, user))
+        self.actions = np.vstack((self.actions, action))
+        self.outcome = np.vstack((self.outcome, outcome))
+
+
+class HistoricalRecommender:
+
+    #################################
+    # Initialise
+    #
+    # Set the recommender with a default number of actions and outcomes.  This is
+    # because the number of actions in historical data can be
+    # different from the ones that you can take with your policy.
+    def __init__(self, n_actions, n_outcomes):
+        self.n_actions = n_actions
+        self.n_outcomes = n_outcomes
+        self.reward = self._default_reward
+
+    # By default, the reward is just equal to the outcome, as the actions play no role.
+    def _default_reward(self, action, outcome):
+        return outcome
+
+    # Set the reward function r(a, y)
+    def set_reward(self, reward):
+        self.reward = reward
+
+    ##################################
+    # Fit a model from patient data.
+    #
+    # This will generally speaking be an
+    # unsupervised model. Anything from a Gaussian mixture model to a
+    # neural network is a valid choice.  However, you can give special
+    # meaning to different parts of the data, and use a supervised
+    # model instead.
+    def fit_data(self, data):
+        print("Preprocessing data")
+        return None
+
+    # Fit a model from patient data, actions and their effects
+    # Here we assume that the outcome is a direct function of data and actions
+    # This model can then be used in estimate_utility(), predict_proba() and recommend()
+    def fit_treatment_outcome(self, data, actions, outcome, recommender_model=None):
+        """Calculates and fits:
+
+        P(y_t | a_t, x_t): the distribution of outcomes
+        pi(a_t | x_t): the distribution of actions
+
+        Args:
+            data: the covariates of the historical data
+            actions: the vector of actions a_t
+            outcome: the vector of outcomes y_t
+        """
+        print("Fitting treatment outcomes")
+
+        x = data.copy()
+        if isinstance(data, pd.DataFrame):
+            x = x.to_numpy()
+
+        if isinstance(actions, pd.DataFrame):
+            actions = actions.to_numpy()
+
+        if isinstance(outcome, pd.DataFrame):
+            outcome = outcome.to_numpy()
+
+        self.data = x
+        self.actions = actions
+        self.outcome = outcome
+        x = np.hstack((x, actions))
+
+        if recommender_model is None:
+            recommender_model = Approach1_hist_bl(
+                self.n_actions, self.n_outcomes)
+
+        recommender_model.fit_treatment_outcome(data, actions, outcome)
+        recommender_model.set_reward(self.reward)
+        self.recommender_model = recommender_model
+
+    # Estimate the utility of a specific policy from historical data (data, actions, outcome),
+    # where utility is the expected reward of the policy.
+    ##
+    # If policy is not given, simply use the average reward of the observed actions and outcomes.
+    ##
+    # If a policy is given, then you can either use importance
+    # sampling, or use the model you have fitted from historical data
+    # to get an estimate of the utility.
+    ##
+    # The policy should be a recommender that implements get_action_probability()
+    def estimate_utility(self, data, actions, outcome, policy=None):
+        """Estimates the expected utility for the provided data set.
+
+        Args:
+            data: covariates of observations
+            actions: vector of action taken for the observations
+            outcome: vector of outcomes for the observations
+
+        Returns:
+            The estimated expected utility.
+        """
+        # if policy is not given, use the class' own get_action_probabilities
+        if policy is None:
+            action_prob_method = self.get_action_probabilities
+        # else, use the provided policy
+        else:
+            action_prob_method = policy.get_action_probabilities
+
+        if isinstance(data, pd.DataFrame):
+            data = data.to_numpy()
+
+        T = len(actions)
+        print(f"Estimating = {T} observations")
+
+        utility = np.zeros(T)
+
+        for t in range(T):
+            # one observation
+            user_data = data[t, ]
+
+            # action distribution
+            pi_a_x = action_prob_method(user_data)
+
+            # expected reward
+            utility[t] = self.estimate_expected_reward(user_data, pi_a_x)
+
+        return np.mean(utility)
+
+    def estimate_expected_reward(self, user_data, pi):
+        """Estimates the expected reward for a given observation.
+
+        Args:
+            user_data: the observation data (x_t)
+            pi: the conditional distribution pi(a_t | x_t)
+
+        Returns:
+            The expected reward for the observation.
+        """
+        estimated_utility = 0
+
+        for a_t in range(self.n_actions):
+
+            for y_t in range(self.n_outcomes):
+                p_y = self.predict_proba(user_data.copy(), a_t)
+
+                estimated_utility += pi[a_t] * p_y[y_t] * self.reward(a_t, y_t)
+
+        return estimated_utility
+
+    # Return a distribution of effects for a given person's data and a specific treatment.
+    # This should be an numpy.array of length self.n_outcomes
+    def predict_proba(self, data, treatment):
+        """Calculates the conditional probability P(y | a, x) through the 
+        recommender model.
+
+        Args:
+            data: the covariates for an observation (x_t)
+            treatment: the action to condition on (a_t)
+
+        Returns:
+            The conditional distribution.
+        """
+        if isinstance(data, pd.core.series.Series):
+            data = data.to_numpy()
+
+        return self.recommender_model.predict_proba(data, treatment)
+
+    # Return a distribution of recommendations for a specific user datum
+    # This should a numpy array of size equal to self.n_actions, summing up to 1
+    def get_action_probabilities(self, user_data):
+        """Calculates the conditional distribution of actions pi(a_t | x_t).
+
+        Args:
+            user_data: observation to calculate the conditional distribution for             
+        """
+        return self.recommender_model.get_action_probabilities(user_data)
+
+    # Return recommendations for a specific user datum
+    # This should be an integer in range(self.n_actions)
+
+    def recommend(self, user_data):
+        return np.random.choice(self.n_actions, p=self.get_action_probabilities(user_data))
+
+    # Observe the effect of an action. This is an opportunity for you
+    # to refit your models, to take the new information into account.
+    def observe(self, user, action, outcome):
+        """Observe new observations dynamically. The historical recommender
+        will not utilize this, but saves the data.
+
+        Args:
+            user: covariates for a specific user
+            action: the action selected by the recommender/policy
+            outcome: the outcome y | a
+        """
+        self.recommender_model.observe(user, action, outcome)
+
+    # After all the data has been obtained, do a final analysis. This can consist of a number of things:
+    # 1. Recommending a specific fixed treatment policy
+    # 2. Suggesting looking at specific genes more closely
+    # 3. Showing whether or not the new treatment might be better than the old, and by how much.
+    # 4. Outputting an estimate of the advantage of gene-targeting treatments versus the best fixed treatment
+    def final_analysis(self, n_tests=1000, generator=None):
+        """After the testing and the new observations are added.
+
+        Args:
+            n_tests: number of tests to run
+            generator: the generator from Christos' test bench
+        """
+        analysis = FinalAnalysis()
+        # 1
+        print(f"1. Specific fixed treatment policy")
+        action_utilities = analysis.fixed_treatment_policy_check(
+            recommender=self, n_tests=n_tests, generator=generator)
+        for a_t in range(self.n_actions):
+            print(f"E[U | a = {a_t}] = {round(action_utilities[a_t, 1], 4)}")
+
+        # 2
+        print(f"2. Looking at specific genes more closely")
+
+        return None
+
+
+if __name__ == "__main__":
+    data = MedicalData()
+    n_actions = len(np.unique(data.a_train))
+    n_outcomes = len(np.unique(data.y_train))
+
+    hist_recommender = HistoricalRecommender(n_actions, n_outcomes)
+    hist_recommender.set_reward(lambda a, y: y - 0.1*(a != 0))
+    hist_recommender.fit_treatment_outcome(
+        data.x_train, data.a_train, data.y_train)
+
+    hist_estimated_utility = hist_recommender.estimate_utility(
+        data.x_test, data.a_test, data.y_test)
+    print(f"Estimated expected utility = {round(hist_estimated_utility, 4)}")
---
+++ src/project-2/part1.py
@@ -0,0 +1,390 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
+from sklearn.feature_selection import RFECV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
+
+
+class MedicalData:
+    def __init__(self):
+        self._get_data()
+
+    def _get_data(self):
+        """Gets the data and places it into three dataframes stored as instance
+        variables of the object.
+        """
+        x = pd.read_csv("./data/medical/historical_X.dat",
+                        sep=" ", header=None)
+        personal_columns = ["sex", "smoker"]
+        gene_columns = ["gene " + str(i) for i in range(1, 127)]
+        symptom_columns = ["symptom 1", "symptom 2"]
+        x_columns = personal_columns + gene_columns + symptom_columns
+        x.columns = x_columns
+
+        y = pd.read_csv("./data/medical/historical_Y.dat",
+                        sep=" ", header=None)
+        y.columns = ["outcome"]
+
+        a = pd.read_csv("./data/medical/historical_A.dat",
+                        sep=" ", header=None)
+        a.columns = ["action"]
+
+        self.x_train, self.x_test, self.y_train, self.y_test, self.a_train, self.a_test = train_test_split(
+            x, y, a, test_size=0.3, random_state=1)
+
+    def data_analysis(self):
+        """
+
+        """
+        self.frequency_symptoms()
+        self.variable_selection(3)
+
+    def _plot_variable_selection(self, accuracy_score1, accuracy_score2, show=False):
+        """Plots the accuracy score for the different symptoms.
+
+        Args:
+            accuracy_score1: the accuracy score from the CV of symptom 1
+            accuracy_score2: the accuracy score from the CV of symptom 2
+            show: whether or not to show the plot
+        """
+        fig, (axis1, axis2) = plt.subplots(1, 2)
+        fig.suptitle("Accuracy score for variable selection")
+
+        axis1.plot(range(1, len(accuracy_score1) + 1), accuracy_score1)
+        axis1.set_title("Symptom 1")
+        axis1.set_ylabel("Accuracy score")
+        axis1.set_xlabel("Number of covariates")
+        axis2.plot(range(1, len(accuracy_score2) + 1), accuracy_score2)
+        axis2.set_title("Symptom 2")
+        axis2.set_xlabel("Number of covariates")
+
+        if show:
+            plt.show()
+        else:
+            plt.savefig("img/var_sel.png")
+
+    def variable_selection(self, num_folds):
+        """Performs variable selection using a num_folds cross-validation.
+
+        RFECV adapted from https://scikit-learn.org/stable/auto_examples/
+        feature_selection/plot_rfe_with_cross_validation.html#sphx-glr-auto-
+        examples-feature-selection-plot-rfe-with-cross-validation-py
+
+        Args:
+            num_folds: the number of folds to use in the cross-validation
+        """
+        logistic_regression = LogisticRegression(max_iter=1000)
+        variable_selection_cv = RFECV(
+            estimator=logistic_regression, step=1, cv=StratifiedKFold(num_folds, random_state=1, shuffle=True), scoring='accuracy')
+
+        x = self.x_train.iloc[:, : -2]
+        symptom1 = self.x_train.iloc[:, -2]
+        symptom2 = self.x_train.iloc[:, -1]
+
+        # symptom 1
+        variable_selection_cv.fit(x, symptom1)
+        accuracy_symptom1 = variable_selection_cv.grid_scores_
+        symptom1_indices = np.where(
+            variable_selection_cv.support_ == True)[0]
+        print(f"Symptom 1 covariates = {x.columns[symptom1_indices]}")
+
+        # symptom 2
+        variable_selection_cv.fit(x, symptom2)
+        accuracy_symptom2 = variable_selection_cv.grid_scores_
+        symptom2_indices = np.where(
+            variable_selection_cv.support_ == True)[0]
+        print(f"Symptom 2 covariates = {x.columns[symptom2_indices]}")
+
+        self._plot_variable_selection(accuracy_symptom1, accuracy_symptom2)
+
+    def frequency_symptoms(self, show=False):
+        """Generates simple histogram showing the frequency of the different 
+        symptoms. Looks at the entire dataset when considering the frequency.
+
+        Args:
+            show: whether or not to show the plot
+        """
+        x_joined = [self.x_train, self.x_test]
+        x = pd.concat(x_joined)
+
+        fig, (axis1, axis2) = plt.subplots(1, 2)
+        fig.suptitle("Histogram of symptoms")
+        axis1.hist(x["symptom 1"], color='b')
+        axis1.set_title("Symptom 1")
+        axis2.hist(x["symptom 2"], color='r')
+        axis2.set_title("Symptom 2")
+
+        if show:
+            plt.show()
+        else:
+            plt.savefig("img/freq_hist.png")
+
+    def measure_effect(self, action):
+        """Calculates the measured effect of an action.
+
+        Args:
+            action: 1 for treatment and 0 for placebo
+
+        Returns:
+            The measured effect.
+        """
+        y_joined = [self.y_train, self.y_test]
+        y = pd.concat(y_joined)
+        y_array = self._to_flat_array(y)
+
+        a_joined = [self.a_train, self.a_test]
+        a = pd.concat(a_joined)
+        a_array = self._to_flat_array(a)
+
+        return self._utility(a_array, y_array, action)
+
+    def measure_effect_symptom(self, action, symptom):
+        """Calculates the measured effect of an action.
+
+        Args:
+            action: 1 for treatment and 0 for placebo
+            symptom: separate observations based on symptom
+
+        Returns:
+            The measured effect.
+        """
+        y_joined = [self.y_train, self.y_test]
+        y = pd.concat(y_joined)
+        y_array = self._to_flat_array(y)
+
+        a_joined = [self.a_train, self.a_test]
+        a = pd.concat(a_joined)
+        a_array = self._to_flat_array(a)
+
+        x_joined = [self.x_train, self.x_test]
+        x = pd.concat(x_joined)
+
+        if symptom == 1:
+            sym_idx = x.iloc[:, -2] == 1
+        else:
+            sym_idx = x.iloc[:, -1] == 1
+
+        a_cond_sym = a_array[sym_idx]
+        y_cond_sym = y_array[sym_idx]
+
+        return self._utility(a_cond_sym, y_cond_sym, action)
+
+    def _to_flat_array(self, df):
+
+        numpy_array = df.to_numpy()
+        return numpy_array.flatten()
+
+    def _utility(self, a, y, at):
+        """Calculates utility.
+
+        Args:
+            a: action array
+            y: outcome array
+            at: action to measure utility for
+
+        Returns:
+            Utility for observation.
+        """
+        num_at = len(np.where(a == at)[0])
+        u = 0
+
+        for i in range(len(a)):
+            if a[i] == at and y[i] == 1:
+                u += 1
+
+        return u/num_at
+
+    def hierarchical_model(self, data, symptom):
+        """Calculates the hierarchical model for the medical data.
+
+        Args:
+            data: the data to calculate the posterior probability
+            symptom: which symptom to use as response variable
+
+        Returns:
+            Posterior probabilites in a Pandas dataframe.
+        """
+        x = data.iloc[:, : -2]
+        if symptom == 1:
+            symptom = data.iloc[:, -2]
+        else:
+            symptom = data.iloc[:, -1]
+
+        num_models = len(x.iloc[0])
+        log_likelihoods = np.zeros(num_models + 1)
+        model = LogisticRegression(max_iter=500)
+
+        for i in range(0, num_models + 1):
+            if i != num_models:
+                single_column = x.iloc[:, i].to_numpy()
+                single_covariate = single_column.reshape(-1, 1)
+                log_reg = model.fit(single_covariate, symptom)
+                p_t = log_reg.predict_proba(single_covariate)
+                log_likelihoods[i] = -log_loss(symptom, p_t)
+            else:
+                log_reg = model.fit(x, symptom)
+                p_t = log_reg.predict_proba(x)
+                log_likelihoods[i] = -log_loss(symptom, p_t)
+
+        # calculating the posterior
+        likelihood = np.exp(log_likelihoods)
+        prior = np.repeat(1/(num_models + 1), num_models + 1)
+        p_y = np.sum(likelihood*prior)
+        posterior = (likelihood*prior)/p_y
+
+        # constructing the dataframe
+        last_index = pd.Index(data=["all"])
+        model_names = x.columns.append(last_index)
+        posterior_df = pd.DataFrame(data=posterior, index=model_names)
+        posterior_df.columns = ["posterior"]
+
+        return posterior_df
+
+    def _calculate_posterior(self, xtrain, xtest, ytrain, ytest):
+        """Calculates the posterior of a test set using a model fitted on 
+        training data.
+
+        Args:
+            xtrain: training covariates
+            xtest: test covariates
+            ytrain: training response
+            ytest: test response
+
+        Returns:
+            The posterior probability of the different models.
+        """
+        num_models = len(xtrain.iloc[0])
+        log_likelihoods = np.zeros(num_models + 1)
+
+        model = LogisticRegression(max_iter=500)
+
+        for i in range(0, num_models + 1):
+            if i != num_models:
+                single_column = xtrain.iloc[:, i].to_numpy()
+                single_covariate = single_column.reshape(-1, 1)
+                log_reg = model.fit(single_covariate, ytrain)
+
+                single_column_test = xtest.iloc[:, i].to_numpy()
+                single_covariate_test = single_column_test.reshape(-1, 1)
+                p_t = log_reg.predict_proba(single_covariate_test)
+
+                log_likelihoods[i] = -log_loss(ytest, p_t)
+            else:
+                log_reg = model.fit(xtrain, ytrain)
+
+                p_t = log_reg.predict_proba(xtest)
+                log_likelihoods[i] = -log_loss(ytest, p_t)
+
+        likelihood = np.exp(log_likelihoods)
+        prior = np.repeat(1/(num_models + 1), num_models + 1)
+        p_y = np.sum(likelihood*prior)
+        posterior = (likelihood*prior)/p_y
+
+        return posterior
+
+    def hierarchical_model_cv(self, symptom, k):
+        """Calculates the hierarchical model for the medical data.
+
+        Args:
+            symptom: which symptom to use as response variable
+            k: the number of folds to use in the cross-validation
+
+        Returns:
+            Posterior probabilites in a Pandas dataframe.
+        """
+
+        num_models = len(self.x_train.iloc[0]) - 1
+        # (folds, models)
+        cv_posterior = np.zeros((k, num_models))
+
+        x_joined = [self.x_train, self.x_test]
+        x_raw = pd.concat(x_joined)
+        x = x_raw.iloc[:, : -2]
+
+        if symptom == 1:
+            y = x.iloc[:, -2]
+        else:
+            y = x.iloc[:, -1]
+
+        kf = KFold(n_splits=k, shuffle=True)
+        k_counter = 0
+
+        for train_indices, test_indices in kf.split(x):
+            xtrain = x.iloc[train_indices, :]
+            ytrain = y[train_indices]
+            xtest = x.iloc[test_indices, :]
+            ytest = y[test_indices]
+
+            posterior = self._calculate_posterior(xtrain, xtest, ytrain, ytest)
+            cv_posterior[k_counter, :] = posterior
+            k_counter += 1
+
+        posterior = np.mean(cv_posterior, 0)
+
+        # constructing the dataframe
+        last_index = pd.Index(data=["all"])
+        model_names = x.columns.append(last_index)
+        posterior_df = pd.DataFrame(data=posterior, index=model_names)
+        posterior_df.columns = ["posterior"]
+
+        return posterior_df
+
+
+def plot_posteriors(posteriors, num, title, show=True):
+    """Plots the top k posterios.
+
+    Args:
+        posteriors: sorted Pandas dataframe with posteriors
+        num: the number of posteriors to plot
+        title: title of the histogram
+        show: whether or not to show the plot
+    """
+    plot_posteriors = posteriors.sort_values(
+        by="posterior", ascending=False)[:num]
+
+    plot_posteriors.plot.bar()
+    plt.title(title)
+    plt.xlabel("covariates")
+    plt.ylabel("P(model | y)")
+
+    if show:
+        plt.show()
+    else:
+        filename = title.replace(" ", "_") + ".png"
+        plt.savefig("img/" + filename)
+
+
+if __name__ == "__main__":
+    data = MedicalData()
+    data.data_analysis()
+    expected_utility_1 = data.measure_effect(1)
+    expected_utility_0 = data.measure_effect(0)
+    print(f"E[U|a_t = 1] = {expected_utility_1}")
+    print(f"E[U|a_t = 0] = {expected_utility_0}")
+
+    util_sym1_a1 = data.measure_effect_symptom(1, 1)
+    util_sym2_a1 = data.measure_effect_symptom(1, 2)
+    util_sym1_a0 = data.measure_effect_symptom(0, 1)
+    util_sym2_a0 = data.measure_effect_symptom(0, 2)
+    print(f"E[U|a_t = 1, sym = 1] = {util_sym1_a1}")
+    print(f"E[U|a_t = 1, sym = 2] = {util_sym2_a1}")
+    print(f"E[U|a_t = 0, sym = 1] = {util_sym1_a0}")
+    print(f"E[U|a_t = 0, sym = 2] = {util_sym2_a0}")
+
+    x_joined = [data.x_train, data.x_test]
+    x = pd.concat(x_joined)
+    sym1_posteriors = data.hierarchical_model(x, 1)
+    plot_posteriors(sym1_posteriors, 5, "histogram for symptom 1", show=False)
+
+    sym2_posteriors = data.hierarchical_model(x, 2)
+    plot_posteriors(sym2_posteriors, 5, "histogram for symptom 2", show=False)
+
+    sym1_cv_posteriors = data.hierarchical_model_cv(1, 5)
+    plot_posteriors(sym1_cv_posteriors, 5,
+                    "histogram cv symptom 1", show=False)
+
+    sym2_cv_posteriors = data.hierarchical_model_cv(2, 5)
+    plot_posteriors(sym2_cv_posteriors, 5,
+                    "histogram cv symptom 2", show=False)
